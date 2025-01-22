064e04f32aeaa42389386b4785d79054715eb9f5
vgont <vgontdev@gmail.com>
2025-01-22 19:12:42 -0300

    feat (room_registry_test): create room test

---
+++ apps/temere_server/test/temere_server/room_registry_test.exs
@@ -0,0 +1,17 @@
+defmodule TemereServer.RoomRegistryTest do
+  alias TemereServer.Player
+  use ExUnit.Case, async: true
+
+  setup do
+    registry = start_supervised!(RoomRegistry)
+    player1 = Player.create!("vinicinho")
+    player2 = Player.create!("nocettinha")
+    %{registry: registry, player1: player1, player2: player2}
+  end
+
+  test "Creates a room.", %{registry: registry, player1: player1} do
+    RoomRegistry.create(registry, player1, "first_room")
+    assert {:ok, _room} = RoomRegistry.lookup(registry, "first_room")
+    # assert {:error, _reason} = RoomRegistry.create(registry, player1, "first_room")
+  end
+end
