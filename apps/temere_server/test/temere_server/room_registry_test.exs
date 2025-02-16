310a669c0f5b4e1572f9ab3380b3176f2befa1d6
vgont <vgontdev@gmail.com>
2025-02-16 18:05:31 -0300

    feat (room_registry_test): delete & lookup tests

--- apps/temere_server/test/temere_server/room_registry_test.exs
+++ apps/temere_server/test/temere_server/room_registry_test.exs
@@ -9,9 +9,20 @@ defmodule TemereServer.RoomRegistryTest do
     %{registry: registry, player1: player1, player2: player2}
   end
 
-  test "Creates a room.", %{registry: registry, player1: player1} do
+  test "Create a room.", %{registry: registry, player1: player1} do
     RoomRegistry.create(registry, player1, "first_room")
     assert {:ok, _room} = RoomRegistry.lookup(registry, "first_room")
-    # assert {:error, _reason} = RoomRegistry.create(registry, player1, "first_room")
+    assert {:error, _reason} = RoomRegistry.create(registry, player1, "first_room")
+  end
+
+  test "Lookup a room.", %{registry: registry, player1: player1} do
+    RoomRegistry.create(registry, player1, "first_room")
+    assert {:ok, _room} = RoomRegistry.lookup(registry, "first_room")
+  end
+
+  test "Delete a room.", %{registry: registry, player1: player1} do
+    RoomRegistry.create(registry, player1, "first_room")
+    RoomRegistry.delete(registry, "first_room")
+    assert {:error, _reason} = RoomRegistry.lookup(registry, "first_room")
   end
 end
