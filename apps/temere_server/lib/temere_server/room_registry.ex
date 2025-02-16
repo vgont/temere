2640e22f3e0e2e7f4d3fad6fae530f94482868ae
vgont <vgontdev@gmail.com>
2025-02-16 18:06:53 -0300

    feat (room_registry): delete method

--- apps/temere_server/lib/temere_server/room_registry.ex
+++ apps/temere_server/lib/temere_server/room_registry.ex
@@ -15,6 +15,10 @@ defmodule RoomRegistry do
     GenServer.call(server, {:lookup, room_name})
   end
 
+  def delete(server, room_name) do
+    GenServer.call(server, {:delete, room_name})
+  end
+
   def init(room_table) do
     :ets.new(room_table, [:set, :protected, :named_table])
     {:ok, room_table}
@@ -37,5 +41,10 @@ defmodule RoomRegistry do
       [{^room_name, room}] -> {:reply, {:ok, room}, room_table}
       [] -> {:reply, {:error, :not_found}, room_table}
     end
+  end
+
+  def handle_call({:delete, room}, _from, room_table) do
+    :ets.delete(room_table, room)
+    {:reply, :ok, room_table}
   end
 end
