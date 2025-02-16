e8d1e480416956ef270211f2703c8b5b0b3f18d4
vgont <vgontdev@gmail.com>
2025-02-16 19:28:57 -0300

    feat (room_test): test for no players in room

--- apps/temere_server/test/temere_server/room_test.exs
+++ apps/temere_server/test/temere_server/room_test.exs
@@ -6,7 +6,7 @@ defmodule TemereServer.RoomTest do
   setup context do
     player_1 = Player.create!("vinicinho")
     GenServer.start_link(Room, player_1, name: context.test)
-    %{room: context.test}
+    %{room: context.test, player_1: player_1}
   end
 
   test "A player joins the room. Room can hold only 2 players", %{room: room} do
@@ -20,10 +20,11 @@ defmodule TemereServer.RoomTest do
     assert {:error, :full_room} = Room.join(room, player)
   end
 
-  test "A player exits the room", %{room: room} do
+  test "A player exits the room", %{room: room, player_1: player_1} do
     player = Player.create!("nocettinha")
     {:ok, _players} = Room.join(room, player)
 
     assert :ok = Room.exit(room, player)
+    assert :ok = Room.exit(room, player_1)
   end
 end
