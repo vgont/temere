4e041c2334ebd552510f1f12f88caf9f645afdd8
vgont <vgontdev@gmail.com>
2025-02-18 06:10:43 -0300

    feat (temere_server.player_router_test): test post /player/new

---
+++ apps/temere_server/test/temere_server/player_router_test.exs
@@ -0,0 +1,17 @@
+defmodule PlayerRouterTest do
+  use ExUnit.Case, async: true
+  use Plug.Test
+  alias TemereServer.Player
+
+  @opts TemereServer.init([])
+ 
+  test "POST /player/new" do
+    conn = conn(:post, "/player/new", %{name: "vgont"})
+
+    conn = TemereServer.call(conn, @opts)
+    assert conn.status == 200
+
+    player = Poison.decode!(conn.resp_body, as: %Player{})
+    assert player.name == "vgont"
+  end
+end
