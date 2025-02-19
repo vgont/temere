409474359022fdb52f91b71b0c85c417a50b46d6
vgont <vgontdev@gmail.com>
2025-02-18 23:43:29 -0300

    feat (temere_server.room_registry_test): change registry to RoomRegistry itself

--- apps/temere_server/test/temere_server/room_registry_test.exs
+++ apps/temere_server/test/temere_server/room_registry_test.exs
@@ -1,22 +1,22 @@
 defmodule TemereServer.RoomRegistryTest do
   alias TemereServer.Player
+  alias TemereServer.RoomRegistry
   use ExUnit.Case, async: true
 
   setup do
-    registry = start_supervised!(RoomRegistry)
     player1 = Player.create!("vinicinho")
     player2 = Player.create!("nocettinha")
-    %{registry: registry, player1: player1, player2: player2}
+    %{player1: player1, player2: player2}
   end
 
-  test "Create a room.", %{registry: registry, player1: player1} do
-    RoomRegistry.create(registry, player1, "first_room")
-    assert {:ok, _room} = RoomRegistry.lookup(registry, "first_room")
-    assert {:error, _reason} = RoomRegistry.create(registry, player1, "first_room")
+  test "Create a room.", %{player1: player1} do
+    RoomRegistry.create(RoomRegistry, player1, "first_room")
+    assert {:ok, _room} = RoomRegistry.lookup(RoomRegistry, "first_room")
+    assert {:error, _reason} = RoomRegistry.create(RoomRegistry, player1, "first_room")
   end
 
-  test "Lookup a room.", %{registry: registry, player1: player1} do
-    RoomRegistry.create(registry, player1, "first_room")
-    assert {:ok, _room} = RoomRegistry.lookup(registry, "first_room")
+  test "Lookup a room.", %{player1: player1} do
+    RoomRegistry.create(RoomRegistry, player1, "first_room")
+    assert {:ok, _room} = RoomRegistry.lookup(RoomRegistry, "first_room")
   end
 end
