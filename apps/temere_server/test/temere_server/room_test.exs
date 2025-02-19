7ce974895ea8e3647fdd901e6256699914f049f8
vgont <vgontdev@gmail.com>
2025-02-18 23:14:33 -0300

    feat (temere_server.room_test): replace "registry" with RoomRegistry itself

--- apps/temere_server/test/temere_server/room_test.exs
+++ apps/temere_server/test/temere_server/room_test.exs
@@ -1,4 +1,5 @@
 defmodule TemereServer.RoomTest do
+  alias TemereServer.RoomRegistry
   alias TemereServer.Player
   alias TemereServer.Room
   use ExUnit.Case, async: true
@@ -20,11 +21,14 @@ defmodule TemereServer.RoomTest do
     assert {:error, :full_room} = Room.join(room, player)
   end
 
-  test "A player exits the room", %{room: room, player_1: player_1} do
+  test "A player exits the room", %{player_1: player_1} do
     player = Player.create!("nocettinha")
-    {:ok, _players} = Room.join(room, player)
 
-    assert :ok = Room.exit(room, player)
+    assert :ok = RoomRegistry.create(RoomRegistry, player_1, "xesquedele")
+    {:ok, room} = RoomRegistry.lookup(RoomRegistry, "xesquedele")
+
     assert :ok = Room.exit(room, player_1)
+
+    assert {:error, :not_found} = RoomRegistry.lookup(RoomRegistry, "xesquedele")
   end
 end
