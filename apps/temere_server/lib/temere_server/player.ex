ccefb763c1e250792967f822b24da3b6bad2f77a
vgont <vgontdev@gmail.com>
2025-02-21 12:09:02 -0300

    feat! (TemereServer.Player): remove type from Player struct

--- apps/temere_server/lib/temere_server/player.ex
+++ apps/temere_server/lib/temere_server/player.ex
@@ -3,12 +3,6 @@ defmodule TemereServer.Player do
   @derive [Poison.Encoder]
   defstruct [:uuid, :name, :type]
 
-  @type t() :: %Player{
-          uuid: String.t(),
-          name: String.t(),
-          type: atom() | nil
-        }
-
   def create!(name) when is_binary(name) do
     %Player{uuid: UUID.uuid4(), name: name}
   end
