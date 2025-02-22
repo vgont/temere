219dc3a7b46bbadf790c8729dd3fc9501f9f2379
vgont <vgontdev@gmail.com>
2025-02-22 16:36:29 -0300

    refactor (TemereServer.RoomTest): test room join and exit

--- apps/temere_server/test/temere_server/room_test.exs
+++ apps/temere_server/test/temere_server/room_test.exs
@@ -10,6 +10,7 @@ defmodule TemereServer.RoomTest do
     %{room: context.test, player_1: player_1}
   end
 
+  @tag :room
   test "A player joins the room. Room can hold only 2 players", %{room: room} do
     player = Player.create!("nocettinha")
     {:ok, players} = Room.join(room, player)
@@ -17,13 +18,12 @@ defmodule TemereServer.RoomTest do
     assert Enum.at(players, 0).name == "nocettinha"
     assert Enum.at(players, 1).name == "vinicinho"
 
-    player = Player.create!("invalid_player")
-    assert {:error, :full_room} = Room.join(room, player)
+    invalid_player = Player.create!("invalid_player")
+    assert {:error, :full_room} = Room.join(room, invalid_player)
   end
 
+  @tag :room
   test "A player exits the room", %{player_1: player_1} do
-    player = Player.create!("nocettinha")
-
     assert :ok = RoomRegistry.create(RoomRegistry, player_1, "xesquedele")
     {:ok, room} = RoomRegistry.lookup(RoomRegistry, "xesquedele")
 
