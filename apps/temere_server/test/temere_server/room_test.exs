45d6c5f840d0bd3169314b7fa5422eedacd020c4
vgont <vgontdev@gmail.com>
2025-02-24 09:38:38 -0300

    feat (TemereServer.Room): tests for Room.get_helper_and_guesser/1, Room.change_helper/1, Room.set_word/2, Room.get_word/1

--- apps/temere_server/test/temere_server/room_test.exs
+++ apps/temere_server/test/temere_server/room_test.exs
@@ -6,17 +6,14 @@ defmodule TemereServer.RoomTest do
 
   setup context do
     player_1 = Player.create!("vinicinho")
+    player_2 = Player.create!("nocettinha")
     GenServer.start_link(Room, player_1, name: context.test)
-    %{room: context.test, player_1: player_1}
+    %{room: context.test, player_1: player_1, player_2: player_2}
   end
 
   @tag :room
-  test "A player joins the room. Room can hold only 2 players", %{room: room} do
-    player = Player.create!("nocettinha")
-    {:ok, players} = Room.join(room, player)
-
-    assert Enum.at(players, 0).name == "nocettinha"
-    assert Enum.at(players, 1).name == "vinicinho"
+  test "A player joins the room. Room can hold only 2 players", %{room: room, player_2: player_2} do
+    assert :ok = Room.join(room, player_2)
 
     invalid_player = Player.create!("invalid_player")
     assert {:error, :full_room} = Room.join(room, invalid_player)
@@ -30,5 +27,26 @@ defmodule TemereServer.RoomTest do
     assert :ok = Room.exit(room, player_1)
 
     assert {:error, :not_found} = RoomRegistry.lookup(RoomRegistry, "xesquedele")
+  end
+
+  @tag :room
+  test "A player can change the helper", %{room: room, player_2: player_2} do
+    Room.join(room, player_2)
+
+    assert {:ok, old_helper, old_guesser} = Room.get_helper_and_guesser(room)
+    assert {:ok, new_helper, new_guesser} = Room.change_helper(room)
+
+    assert old_helper == new_guesser
+    assert old_guesser == new_helper
+  end
+
+  @tag :room
+  test "Player SET and GET a word to the room", %{room: room} do
+    assert {:ok, nil} = Room.get_word(room)
+
+    assert :ok = Room.set_word(room, "word")
+
+    assert {:ok, word} = Room.get_word(room)
+    assert word == "word"
   end
 end
