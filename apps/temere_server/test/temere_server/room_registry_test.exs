99d61565350c969677df4b9fc10051c7b75f8877
vgont <vgontdev@gmail.com>
2025-02-24 09:43:49 -0300

    feat (TemereServer.RoomRegistry): tag :room_registry

--- apps/temere_server/test/temere_server/room_registry_test.exs
+++ apps/temere_server/test/temere_server/room_registry_test.exs
@@ -4,25 +4,28 @@ defmodule TemereServer.RoomRegistryTest do
   use ExUnit.Case, async: true
 
   setup do
-    player1 = Player.create!("vinicinho")
-    player2 = Player.create!("nocettinha")
-    %{player1: player1, player2: player2}
+    player_1 = Player.create!("vinicinho")
+    player_2 = Player.create!("nocettinha")
+    %{player_1: player_1, player_2: player_2}
   end
 
-  test "Create a room.", %{player1: player1} do
-    RoomRegistry.create(RoomRegistry, player1, "first_room")
+  @tag :room_registry
+  test "Create a room.", %{player_1: player_1} do
+    RoomRegistry.create(RoomRegistry, player_1, "first_room")
     assert {:ok, _room} = RoomRegistry.lookup(RoomRegistry, "first_room")
-    assert {:error, _reason} = RoomRegistry.create(RoomRegistry, player1, "first_room")
+    assert {:error, _reason} = RoomRegistry.create(RoomRegistry, player_1, "first_room")
   end
 
-  test "Lookup a room.", %{player1: player1} do
-    RoomRegistry.create(RoomRegistry, player1, "first_room")
+  @tag :room_registry
+  test "Lookup a room.", %{player_1: player_1} do
+    RoomRegistry.create(RoomRegistry, player_1, "first_room")
     assert {:ok, _room} = RoomRegistry.lookup(RoomRegistry, "first_room")
   end
 
-  test "Get all rooms.", %{player1: player1} do
-    RoomRegistry.create(RoomRegistry, player1, "first_room")
-    RoomRegistry.create(RoomRegistry, player1, "second_room")
+  @tag :room_registry
+  test "Get all rooms.", %{player_1: player_1} do
+    RoomRegistry.create(RoomRegistry, player_1, "first_room")
+    RoomRegistry.create(RoomRegistry, player_1, "second_room")
     assert rooms = RoomRegistry.get_all_rooms(RoomRegistry)
     assert length(rooms) > 0
   end
