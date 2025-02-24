5888ba0d04b43eb5d5c62d3b6d066def11d19066
vgont <vgontdev@gmail.com>
2025-02-24 06:44:56 -0300

    feat! (TemereServer.Player): remove :type from Player struct

--- apps/temere_server/lib/temere_server/player.ex
+++ apps/temere_server/lib/temere_server/player.ex
@@ -1,7 +1,7 @@
 defmodule TemereServer.Player do
   alias __MODULE__
   @derive [Poison.Encoder]
-  defstruct [:uuid, :name, :type]
+  defstruct [:uuid, :name]
 
   def create!(name) when is_binary(name) do
     %Player{uuid: UUID.uuid4(), name: name}
