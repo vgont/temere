b6b4177569bd1e398ba29abedc3d386445014010
vgont <vgontdev@gmail.com>
2025-02-25 14:16:47 -0300

    refactor (TemereServer.RoomRegistryTest): test Join a room.

--- apps/temere_server/test/temere_server/room_registry_test.exs
+++ apps/temere_server/test/temere_server/room_registry_test.exs
@@ -17,16 +17,16 @@ defmodule TemereServer.RoomRegistryTest do
   end
 
   @tag :room_registry
-  test "Lookup a room.", %{player_1: player_1} do
-    RoomRegistry.create(RoomRegistry, player_1, "first_room")
-    assert {:ok, _room} = RoomRegistry.lookup(RoomRegistry, "first_room")
-  end
-
-  @tag :room_registry
   test "Get all rooms.", %{player_1: player_1} do
-    RoomRegistry.create(RoomRegistry, player_1, "first_room")
     RoomRegistry.create(RoomRegistry, player_1, "second_room")
     assert rooms = RoomRegistry.get_all_rooms(RoomRegistry)
     assert length(rooms) > 0
+  end
+
+  @tag :room_registry
+  test "Join a room.", %{player_1: player_1, player_2: player_2} do
+    RoomRegistry.create(RoomRegistry, player_1, "joinar")
+    assert {:error, _} = RoomRegistry.join(RoomRegistry, "joinar", player_1)
+    assert :ok = RoomRegistry.join(RoomRegistry, "joinar", player_2)
   end
 end
