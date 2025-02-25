b4402e26b1f9b54ed5f85c3c751234796b86a416
vgont <vgontdev@gmail.com>
2025-02-25 14:14:50 -0300

    feat (TemereServer.RoomRegistryRouter): setup and test POST /room/join/:room_name

--- apps/temere_server/test/temere_server/room_registry_router_test.exs
+++ apps/temere_server/test/temere_server/room_registry_router_test.exs
@@ -6,8 +6,14 @@ defmodule TemereServer.RoomRegistryRouterTest do
 
   @opts TemereServer.init([])
 
-  test "POST /room/new" do
-    player = Player.create!("vgont")
+  setup do
+    player_1 = Player.create!("vgont")
+    player_2 = Player.create!("nocettinha")
+    %{player_1: player_1, player_2: player_2}
+  end
+
+  @tag :room_registry_router
+  test "POST /room/new", %{player_1: player} do
     conn = conn(:post, "/room/new", %{player: player, room_name: "namae"})
 
     conn = TemereServer.call(conn, @opts)
@@ -15,8 +21,8 @@ defmodule TemereServer.RoomRegistryRouterTest do
     assert {:ok, _room} = RoomRegistry.lookup(RoomRegistry, "namae")
   end
 
-  test "GET /room/all" do
-    player = Player.create!("vgont")
+  @tag :room_registry_router
+  test "GET /room/all", %{player_1: player} do
     RoomRegistry.create(RoomRegistry, player, "manae")
 
     conn = conn(:get, "/room/all")
@@ -24,5 +30,18 @@ defmodule TemereServer.RoomRegistryRouterTest do
     assert conn.status == 200
     [room_name | _] = Poison.decode!(conn.resp_body)
     assert room_name == "manae"
+  end
+
+  @tag :room_registry_router
+  test "POST /room/join/:room_name", %{player_1: player_1, player_2: player_2} do
+    {:ok, _room} = RoomRegistry.create(RoomRegistry, player_1, "joinit")
+
+    conn = conn(:post, "/room/join/joinit", %{player: player_1})
+    conn = TemereServer.call(conn, @opts)
+    assert conn.status == 400
+
+    conn = conn(:post, "/room/join/joinit", %{player: player_2})
+    conn = TemereServer.call(conn, @opts)
+    assert conn.status == 200
   end
 end
