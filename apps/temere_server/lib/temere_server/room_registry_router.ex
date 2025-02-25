e8dc546470c0b152da53682bd7c6bbb79582d309
vgont <vgontdev@gmail.com>
2025-02-25 14:13:31 -0300

    feat (TemereServer.RoomRegistryRouter): all responses are now in json

--- apps/temere_server/lib/temere_server/room_registry_router.ex
+++ apps/temere_server/lib/temere_server/room_registry_router.ex
@@ -9,14 +9,23 @@ defmodule TemereServer.RoomRegistryRouter do
   post "/new" do
     player = conn.body_params["player"]
     room_name = conn.body_params["room_name"]
-    :ok = RoomRegistry.create(TemereServer.RoomRegistry, player, room_name)
-    conn = send_resp(conn, 200, "Joined")
+    {:ok, _room} = RoomRegistry.create(TemereServer.RoomRegistry, player, room_name)
+    conn = send_resp(conn, 200, Poison.encode!(%{message: "Room successfully created"}))
     conn
   end
 
   get "/all" do
-    rooms = inspect RoomRegistry.get_all_rooms(TemereServer.RoomRegistry)
+    rooms = inspect(RoomRegistry.get_all_rooms(TemereServer.RoomRegistry))
     conn = send_resp(conn, 200, rooms)
     conn
+  end
+
+  post "/join/:room_name" do
+    player = conn.body_params["player"]
+    result = RoomRegistry.join(TemereServer.RoomRegistry, room_name, player)
+    case result do
+      :ok -> send_resp(conn, 200, Poison.encode!(%{message: "Player successfully joined"}))
+      {:error, reason} -> send_resp(conn, 400, Poison.encode!(%{message: reason}))
+    end
   end
 end
